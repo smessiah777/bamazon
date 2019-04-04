ed7610b0625bdec740142a8217b602805d7a0fba
roninTDK <smessiah17@gmail.com>
2019-04-04 00:14:00 -0500

    added console.table to the project to display database table and implements inquirer UI

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,9 @@
+///all my awesome co-dependencies uh i mean dependencies ;)
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-// var table = require("table");
+const cTable = require("console.table");
 
+///the stuff to connect to MySQL database Yahtzee!
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -9,46 +11,19 @@ var connection = mysql.createConnection({
   password: "root",
   database: "bamazon"
 });
-
+///The code to make sure the connection to database is...actually happening
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected");
 });
 
+///Calling the function that grabs the database and displays it
 displayTable();
 
-function displayTable() {
-  connection.query("select * from products", function(err, data) {
-    if (err) {
-      console.log(err);
-    }
-
-    console.log(data);
-
-    // config = {
-    //   columns: {
-    //     0: {
-    //       alignment: "left",
-    //       minWidth: 10
-    //     },
-    //     1: {
-    //       alignment: "center",
-    //       minWidth: 10
-    //     },
-    //     2: {
-    //       alignment: "right",
-    //       minWidth: 10
-    //     }
-    //   }
-    // };
-
-    // output = table(data, config);
-    // console.log(output);
-  });
-  startMenu();
-}
-
+///Function that holds main menu inquirer UI
 function startMenu() {
+  // console.log("\n ------------------------- \n \n ");
+
   inquirer
     .prompt([
       {
@@ -63,15 +38,18 @@ function startMenu() {
       }
     ])
     .then(answers => {
-      if (answers.prompt1) {
-        connection.query("SELECT id FROM products", function(err, data) {
-          if (err) {
-            console.log(err);
-          }
-        });
-      } else if (answers.prompt2) {
-      }
-      console.log("input entered");
       displayTable();
     });
+}
+
+///Function that grabs and display that sweet sweet database data..yum :P
+function displayTable() {
+  connection.query("select * from products", function(err, data) {
+    if (err) {
+      console.log(err);
+    }
+
+    console.table(data);
+    startMenu();
+  });
 }
